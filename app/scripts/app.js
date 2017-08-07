2cfa61da26b172be623144270bc3423b9c44d852
Даниил Высоцкий <Vysotskiy.D@terempro.loc>
2017-08-07 17:51:24 +0300

    wip

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,10 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 import slick from 'slick-carousel';
+import select2 from 'select2';
 
 $(() => {
 	function initModals() {
-		console.log('modals inited');
 		// Show modal
 		$('[data-link="modal"]').on('click', function () {
 			const link = $(this).attr('href') || $(this).data('href');
@@ -48,7 +48,37 @@ $(() => {
 			slidesToScroll: 4,
 			dots: true,
 			infinite: false,
-			prevArrow: '<button type="button" class="slick-prev slick-arrow--blue">Previous</button>'
+			responsive: [
+				{
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 3,
+						infinite: true,
+						dots: true
+					}
+				},
+				{
+					breakpoint: 600,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2
+					}
+				},
+				{
+					breakpoint: 400,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1
+					}
+				}
+			]
+		});
+	}
+
+	function initSelect2() {
+		$('#choose-city').select2({
+			minimumResultsForSearch: Infinity
 		});
 	}
 
@@ -57,6 +87,18 @@ $(() => {
 		svg4everybody();
 		initModals();
 		initSliders();
+		initSelect2();
+
+		// var docWidth = document.documentElement.offsetWidth;
+
+		// [].forEach.call(
+		// 	document.querySelectorAll('*'),
+		// 	function(el) {
+		// 		if (el.offsetWidth > docWidth) {
+		// 			console.log(el);
+		// 		}
+		// 	}
+		// );
 
 		$(window).on('scroll', megaMenusScrollHandle);
 	});
---
+++ dist/assets/scripts/0.df8117db95b818c40948.hot-update.js
@@ -0,0 +1,83 @@
+webpackHotUpdate(0,[
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	var _svg4everybody = __webpack_require__(1);
+	
+	var _svg4everybody2 = _interopRequireDefault(_svg4everybody);
+	
+	var _jquery = __webpack_require__(2);
+	
+	var _jquery2 = _interopRequireDefault(_jquery);
+	
+	var _slickCarousel = __webpack_require__(5);
+	
+	var _slickCarousel2 = _interopRequireDefault(_slickCarousel);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	(0, _jquery2.default)(function () {
+		function initModals() {
+			console.log('modals inited');
+			// Show modal
+			(0, _jquery2.default)('[data-link="modal"]').on('click', function () {
+				var link = (0, _jquery2.default)(this).attr('href') || (0, _jquery2.default)(this).data('href');
+				var modal = (0, _jquery2.default)(link);
+	
+				(0, _jquery2.default)(modal).addClass('active');
+			});
+			// Hide modal on cross click
+			(0, _jquery2.default)('.modal').on('click', '.close', function () {
+				(0, _jquery2.default)(this).closest('.modal').removeClass('active');
+			});
+			// Hide modal on empty place click
+			(0, _jquery2.default)(document).on('click', function (e) {
+				var target = e.target;
+	
+				if ((0, _jquery2.default)(target).hasClass('modal')) {
+					(0, _jquery2.default)('.modal').removeClass('active');
+				}
+			});
+		}
+	
+		function megaMenusScrollHandle() {
+			var yOffset = -(0, _jquery2.default)(window).scrollTop() + 15;
+			var megaMenus = (0, _jquery2.default)('.js-mega-menu');
+	
+			_jquery2.default.each(megaMenus, function (idx, elem) {
+				if ((0, _jquery2.default)(elem).css('position') === 'fixed') {
+					(0, _jquery2.default)(elem).css('transform', 'translateY(' + yOffset + 'px)');
+				}
+			});
+		}
+	
+		function initSliders() {
+			(0, _jquery2.default)('.slider.single-item').slick({
+				dots: true,
+				infinite: false
+			});
+	
+			(0, _jquery2.default)('.slider.items').slick({
+				slidesToShow: 4,
+				slidesToScroll: 4,
+				dots: true,
+				infinite: false,
+				prevArrow: '<button type="button" class="slick-prev slick-arrow--blue">Previous</button>'
+			});
+		}
+	
+		(0, _jquery2.default)(document).ready(function () {
+			(0, _svg4everybody2.default)();
+			initModals();
+			(0, _slickCarousel2.default)();
+			initSliders();
+	
+			(0, _jquery2.default)(window).on('scroll', megaMenusScrollHandle);
+		});
+	});
+
+/***/ })
+])
+//# sourceMappingURL=0.df8117db95b818c40948.hot-update.js.map
